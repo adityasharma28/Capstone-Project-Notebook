070d836670d36491f4ec507a2c45ac3baa791bc1
adityasharma28 <47492168+adityasharma28@users.noreply.github.com>
2019-06-04 13:18:48 +0530

    Creating jupiter notebook

---
+++ Untitled.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy
+
+
+# In[2]:
+
+
+import pandas
+
+
+# In[3]:
+
+
+import matplotlib
+
+
+# In[3]:
+
+
+get_ipython().run_line_magic('time', '')
+s=0
+for i in range (n):
+    s+=sum
+
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+get_ipython().run_line_magic('matplotlib', 'inline')
+
